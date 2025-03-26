176a8a54b22759d3ca06dc325c241d3ad7d588f6
Humenyuk_Dmytro <humenykdima71@gmail.com>
2025-03-26 12:11:06 +0200

    add new project

---
+++ Lab_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Введіть сторону а");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введіть сторону b");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double search = a * b;
+
+            Console.WriteLine($"Результат: {search}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab_3_V2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_3_V2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(textBox1.Text);
+                double b = double.Parse(textBox2.Text);
+
+                double area = a * b;
+
+                label3.Text = $"Площа: {area}";
+            } catch {
+                MessageBox.Show("Будь ласка, введіть коректні числові значення!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
